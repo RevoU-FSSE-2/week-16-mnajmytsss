615f08de5d64465c1472a87cecd0f9919379ee87
mnajmytsss <najmytsaqiby22@gmail.com>
2023-10-13 17:18:07 +0700

    add auth routes

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,15 @@ const { Router } = require("express");
 const bookRoutes = require("./bookRoutes");
 const dashboardRoutes = require("./dashboardRoutet");
 const preventAttackController = require("./preventAttackRoutes")
+const authRouter = require("./authRoutes")
+const authenticationMiddleware = require("../middleware/authenticationMiddleware")
 
 const router = Router();
 
 router.use("/", dashboardRoutes);
 router.use("/api/v1/attack", preventAttackController)
-router.use("/api/v1/books", bookRoutes);
+router.use("/api/v1", authenticationMiddleware, bookRoutes);
+router.use("/auth", authRouter)
+
 
 module.exports = router;
