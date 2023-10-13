38be9de5a88d62976b45493a75bf7a8a1ec341b3
mnajmytsss <najmytsaqiby22@gmail.com>
2023-10-13 17:15:55 +0700

    add db localhost link

--- middleware/databaseMiddleware.js
+++ middleware/databaseMiddleware.js
@@ -1,10 +1,11 @@
 const dbConnection = require('../db');
 
 const databaseMiddleware = async (req, res, next) => {
- const { db, collection } = await dbConnection();
-req.db = db;
-req.collection = collection; 
-next();
+    const { db, booksCollection, usersCollection } = await dbConnection();
+    req.db = db;
+    req.booksCollection = booksCollection; // Updated to include Books collection
+    req.usersCollection = usersCollection; // Added Users collection
+    next();
 };
 
 module.exports = databaseMiddleware;
