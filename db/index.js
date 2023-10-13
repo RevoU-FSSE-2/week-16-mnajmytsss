e03c03cb57469be9a08a398e6c0aac5ab0e602aa
mnajmytsss <najmytsaqiby22@gmail.com>
2023-10-13 17:16:09 +0700

    add db localhost link

--- db/index.js
+++ db/index.js
@@ -1,12 +1,15 @@
 const { MongoClient } = require('mongodb');
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 
 const dbConnection = async () => {
     try {
-        const client = await new MongoClient(process.env.DB_PROD).connect();
+        const client = await new MongoClient(process.env.DB_DEV).connect();
         const db = client.db('week-15');
-        const collection = db.collection('Books');
-        return { db, collection };
+        
+        const booksCollection = db.collection('Books');
+        const usersCollection = db.collection('Users'); 
+        
+        return { db, booksCollection, usersCollection };
     } catch (error) {
         console.log(error, "<=================== error ==================");
         throw new Error("Database connection error");
@@ -14,3 +17,4 @@ const dbConnection = async () => {
 }
 
 module.exports = dbConnection;
+
