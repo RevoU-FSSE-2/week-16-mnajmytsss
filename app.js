f9119f0fd8e461435d9edea546571f4521a11206
mnajmytsss <najmytsaqiby22@gmail.com>
2023-10-14 07:49:56 +0700

    add tokenCookieMiddleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require ('dotenv')
 const useMiddleware = require('./middleware');
 const router = require('./routes');
+const tokenCookieMiddleware = require('./middleware/tokenCookieMiddleware')
 
 const app = express()
 dotenv.config()
@@ -9,6 +10,7 @@ dotenv.config()
 useMiddleware(app)
 
 app.use(router)
+app.use(tokenCookieMiddleware)
 
 const port = (process.env.PORT) 
 
